ef2d5397a51739264e8404251d18cd9e24c83ffe
nimiabaga <nimiabaga@yahoo.com>
2023-11-30 12:54:13 +0100

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+
+    args = argv[1:]
+    total = 0
+
+    for arg in args:
+        total += int(arg)
+
+    print(total)
