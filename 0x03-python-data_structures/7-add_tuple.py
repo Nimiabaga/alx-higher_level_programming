9ef682d1fd7f9949d27ccaf8e65ce43d02549dbc
nimiabaga <nimiabaga@yahoo.com>
2023-12-03 20:10:03 +0100

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Ensure each tuple has at least 2 elements
-    a = tuple_a + (0, 0)
-    b = tuple_b + (0, 0)
-
-    # Calculate the sum of corresponding elements
-    sum_tuple = (a[0] + b[0], a[1] + b[1])
-
-    return sum_tuple
+    u = len(tuple_a)
+    v = len(tuple_b)
+    size = ((tuple_a[0] if u >= 1 else 0) +
+            (tuple_b[0] if v >= 1 else 0),
+            (tuple_a[1] if u >= 2 else 0) +
+            (tuple_b[1] if v >= 2 else 0))
+    return size
