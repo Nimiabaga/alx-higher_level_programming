2b01d9ac94f8ac7d2b55f09750c3029dc5ad92d9
nimiabaga <nimiabaga@yahoo.com>
2023-12-03 19:54:47 +0100

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
-        return my_list.copy()  # Return a copy of the original list if idx is out of range
+        return my_list.copy()
 
     new_list = my_list.copy()  # Create a copy of the original list
     new_list[idx] = element    # Replace the element at the specified index
