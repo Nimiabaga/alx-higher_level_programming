cfc011841323e11146f4de3268e2e2f20e4b3d9a
nimiabaga <nimiabaga@yahoo.com>
2023-12-03 21:00:10 +0100

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,80 +1,71 @@
 #include "lists.h"
-int is_palindrome(listint_t **head);
-
 
 /**
- * is_palindrome - Checks if a linked list is a palindrome.
- * @head: Pointer to the head of the list.
+ * moveback_listint - Func moves a list back
+ * @head: Head
  *
- * Return: 1 if the list is a palindrome, 0 otherwise.
+ * Return: Ptr to node
+ */
+void moveback_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *new = *head;
+	listint_t *next = NULL;
+
+	while (new)
+	{
+		next = new->next;
+		new->next = prev;
+		prev = new;
+		new = next;
+	}
+
+	*head = prev;
+}
+
+/**
+ * is_palindrome - Func checks list is palindrome
+ * @head: Head
+ *
+ * Return: 1 if it is palindrome, 0 if otherwise
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *runner = *head, *walker = *head, *second_half = *head;
-	listint_t *reversed_half = NULL;
+	listint_t *u = *head, *v = *head, *shrt = *head;
+	listint_t *cpy = NULL;
 
-	/* Check for empty list or single-node list (palindrome) */
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	/* Move runner to the end and walker to the middle of the list */
 	while (1)
 	{
-		runner = runner->next->next;
-		if (!runner)
+		v = v->next->next;
+		if (!v)
 		{
-			reversed_half = walker->next;
+			cpy = u->next;
 			break;
 		}
-		if (!runner->next)
+		if (!v->next)
 		{
-			reversed_half = walker->next->next;
+			cpy = u->next->next;
 			break;
 		}
-		walker = walker->next;
+		u = u->next;
 	}
+	moveback_listint(&cpy);
 
-	/* Reverse the second half of the list */
-	moveback_listint(&reversed_half);
-
-	/* Compare the first and reversed second half of the list */
-	while (reversed_half && second_half)
+	while (cpy && shrt)
 	{
-		if (second_half->n == reversed_half->n)
+		if (shrt->n == cpy->n)
 		{
-			reversed_half = reversed_half->next;
-			second_half = second_half->next;
+			cpy = cpy->next;
+			shrt = shrt->next;
 		}
 		else
 			return (0);
 	}
 
-	/* If reversed_half is NULL, the list is a palindrome */
-	if (!reversed_half)
+	if (!cpy)
 		return (1);
-
 	return (0);
-}
-
-/**
- * reverse_listint - Reverses the direction of a linked list.
- * @head: Pointer to the head of the list.
- *
- * Return: Pointer to the new head of the reversed list.
- */
-void reverse_listint(listint_t **head)
-{
-	listint_t *previous = NULL;
-	listint_t *current = *head;
-	listint_t *next_node = NULL;
-
-	while (current)
-	{
-		next_node = current->next;
-		current->next = previous;
-		previous = current;
-		current = next_node;
-	}
-
-	*head = previous;
 }
