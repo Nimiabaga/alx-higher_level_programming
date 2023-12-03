d602a1ed55c314d2483865265ca2ef2a6ba40d56
nimiabaga <nimiabaga@yahoo.com>
2023-12-03 20:49:24 +0100

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,39 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
-
-/**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: pointer to the head of the list
- * Return: 1 if palindrome, 0 otherwise
- */
-int is_palindrome(listint_t **head)
-{
-	int arr[1000]; /* Assuming a maximum size for the array */
-	int i, j;
-	int length = 0;
-
-	listint_t *current = *head;
-
-	if (current == NULL)
-		return (1);
-
-	/* Store the values of the linked list in the array */
-	while (current != NULL)
-	{
-		arr[length] = current->n;
-		current = current->next;
-		length++;
-	}
-
-	for (i = 0, j = length - 1; i < length / 2; i++, j--)
-	{
-		if (arr[i] != arr[j])
-			return (0);
-	}
-
-	return (1);
-}
 
 /**
  * reverse_listint - Reverses a linked list
@@ -57,3 +22,52 @@ void reverse_listint(listint_t **head)
 
 	*head = previous;
 }
+
+/**
+ * is_palindrome - Checks if a linked list is a palindrome
+ * @head: Pointer to the head of the list
+ *
+ * Return: 1 if it is a palindrome, 0 otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow = *head, *fast = *head, *second_half = *head;
+	listint_t *copy = NULL;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	while (1)
+	{
+		fast = fast->next->next;
+		if (!fast)
+		{
+			copy = slow->next;
+			break;
+		}
+		if (!fast->next)
+		{
+			copy = slow->next->next;
+			break;
+		}
+		slow = slow->next;
+	}
+
+	reverse_listint(&copy);
+
+	while (copy && second_half)
+	{
+		if (second_half->n == copy->n)
+		{
+			copy = copy->next;
+			second_half = second_half->next;
+		}
+		else
+			return (0);
+	}
+
+	if (!copy)
+		return (1);
+	return (0);
+}
+
