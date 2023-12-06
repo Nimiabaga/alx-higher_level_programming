45b1689101259d52c631c23f577dcb57167e9984
nimiabaga <nimiabaga@yahoo.com>
2023-12-06 23:13:13 +0100

    103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,12 +1,12 @@
 #include <Python.h>
 
 /**
- * print_python_bytes - a Functions that prints PY bytes
- * @p: python object
+ * print_python_bytes - Func prints PY bytes
+ * @p: PY object
  */
 void print_python_bytes(PyObject *p)
 {
-	long int element_size
+	long int size;
 	int u;
 	char *str = NULL;
 
@@ -17,33 +17,33 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 
-	PyBytes_AsStringAndSize(p, &str, &element_size);
-	printf("  size: %li\n", element_size);
+	PyBytes_AsStringAndSize(p, &str, &size);
+	printf("  size: %li\n", size);
 	printf("  trying string: %s\n", str);
 	if (size < 10)
-		printf("  first %li bytes:", element_size + 1);
+		printf("  first %li bytes:", size + 1);
 	else
 		printf("  first 10 bytes:");
-	for (u = 0; u <= element_size && u < 10; u++)
+	for (u = 0; u <= size && u < 10; u++)
 		printf(" %02hhx", str[u]);
 	printf("\n");
 }
 
 /**
- * print_python_list - a Function that prints PY list
+ * print_python_list - Func prints PY list
  * @p: PY object
  */
 void print_python_list(PyObject *p)
 {
-	long int  element_size = PyList_Size(p);
+	long int size = PyList_Size(p);
 	int u;
 	PyListObject *list = (PyListObject *)p;
 	const char *obj;
 
 	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %li\n", element_size);
+	printf("[*] Size of the Python List = %li\n", size);
 	printf("[*] Allocated = %li\n", list->allocated);
-	for (u = 0; u < element_size; u++)
+	for (u = 0; u < size; u++)
 	{
 		obj = (list->ob_item[u])->ob_type->tp_name;
 		printf("Element %i: %s\n", u, obj);
