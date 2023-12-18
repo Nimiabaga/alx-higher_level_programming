ad4da7c00a55fafe1abd2102b9eb076ecb750c14
nimiabaga <nimiabaga@yahoo.com>
2023-12-18 20:12:58 +0100

    103-python.c

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -5,13 +5,13 @@ void print_python_bytes(PyObject *obj);
 void print_python_list(PyObject *obj);
 
 /**
- * print_python_float - Print Python float object info
- * @obj: PyObject pointer
+ * print_python_float - Print information about a Python float object
+ * @obj: PyObject pointer representing a Python float
  */
 void print_python_float(PyObject *obj)
 {
 	double value = 0;
-	char *str_repr = NULL;
+	char *string_representation = NULL;
 
 	fflush(stdout);
 	printf("[.] float object info\n");
@@ -23,14 +23,14 @@ void print_python_float(PyObject *obj)
 	}
 
 	value = ((PyFloatObject *)obj)->ob_fval;
-	str_repr = PyOS_double_to_string(value, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
-	printf("  value: %s\n", str_repr);
-	PyMem_Free(str_repr);
+	string_representation = PyOS_double_to_string(value, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
+	printf("  value: %s\n", string_representation);
+	PyMem_Free(string_representation);
 }
 
 /**
- * print_python_bytes - Print Python bytes object info
- * @obj: PyObject pointer
+ * print_python_bytes - Print information about a Python bytes object
+ * @obj: PyObject pointer representing a Python bytes object
  */
 void print_python_bytes(PyObject *obj)
 {
@@ -62,44 +62,4 @@ void print_python_bytes(PyObject *obj)
 			index++;
 		}
 
-		printf("\n");
-	}
-}
-
-/**
- * print_python_list - Print Python list object info
- * @obj: PyObject pointer
- */
-void print_python_list(PyObject *obj)
-{
-	Py_ssize_t length = 0;
-	PyObject *element;
-	int index = 0;
-
-	fflush(stdout);
-	printf("[*] Python list info\n");
-
-	if (PyList_CheckExact(obj))
-	{
-		length = PyList_GET_SIZE(obj);
-		printf("[*] Size of the Python List = %zd\n", length);
-		printf("[*] Allocated = %lu\n", ((PyListObject *)obj)->allocated);
-
-		while (index < length)
-		{
-			element = PyList_GET_ITEM(obj, index);
-			printf("Element %d: %s\n", index, element->ob_type->tp_name);
-
-			if (PyBytes_Check(element))
-				print_python_bytes(element);
-			else if (PyFloat_Check(element))
-				print_python_float(element);
-
-			index++;
-		}
-	}
-	else
-	{
-		printf("  [ERROR] Invalid List Object\n");
-	}
-}
+		printf("\
