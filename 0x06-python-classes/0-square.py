fc0c2a20836ea1d960506180c72409fa5e93beaa
nimiabaga <nimiabaga@yahoo.com>
2023-12-19 14:31:25 +0100

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
+"""square module"""
+
 
 class Square:
+    """Square representation"""
     pass
-
-if __name__ == "__main__":
-    my_square = Square()
-    print(type(my_square))
-    print(my_square.__dict__)
