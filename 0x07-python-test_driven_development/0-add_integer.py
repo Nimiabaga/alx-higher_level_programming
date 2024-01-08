4eca4e5694b7a7ab54916e7d4279728b70df4e28
nimiabaga <nimiabaga@yahoo.com>
2024-01-08 11:55:52 +0100

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,10 +2,10 @@
 """function that adds 2 integers"""
 
 
-def add_integer(c, d=98):
-    """Returns an integer: the addition of c and d"""
-    if ((not isinstance(c, int) and not isinstance(c, float))):
-        raise TypeError("c must be an integer")
-    if ((not isinstance(d, int) and not isinstance(d, float))):
-        raise TypeError("d must be an integer")
-    return int(c) + int(d)
+def add_integer(a, b=98):
+    """Returns an integer: the addition of a and b"""
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
