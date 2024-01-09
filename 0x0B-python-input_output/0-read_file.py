0ac06eb69badaa51023a72c9e7000fe1207655a9
nimiabaga <nimiabaga@yahoo.com>
2024-01-09 12:52:05 +0100

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""func reads a text file (UTF8) and prints it to stdout"""
+
 
 def read_file(filename=""):
-    """Prints the content of a text file to stdout."""
-    with open(filename, encoding="utf-8") as file:
-        print(file.read(), end="")
+    """Prints the content to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
