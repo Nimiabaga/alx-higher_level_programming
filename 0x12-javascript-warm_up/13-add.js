88f0a0fc12b9ea5dc1b48494bad10fdd0fcada87
nimiabaga <nimiabaga@yahoo.com>
2024-03-11 22:50:49 +0100

    13-add.js

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
 module.exports = {
-    // Takes two parameters 'a' and 'b' and returns their sum
-    add: (a, b) => a + b
-  };
-  
+  // Takes two parameters 'a' and 'b' and returns their sum
+  add: (a, b) => a + b
+};
