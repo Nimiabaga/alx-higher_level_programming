3f07a856a4faa85573acde13305bed65e83c95b0
nimiabaga <nimiabaga@yahoo.com>
2024-03-11 22:19:29 +0100

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,25 @@
 #!/usr/bin/node
 
 // Define func 'add' that takes two param. 'a' and 'b' and returns the sum
-function add (a, b) {
+function add(a, b) {
+    // Check if 'a' and 'b' are valid integers
+    if (Number.isNaN(a) || Number.isNaN(b)) {
+      console.log('Invalid input. Please provide two valid integers.');
+      return;
+    }
+  
     return a + b;
   }
   
-  console.log(
-    add(Number.parseInt(process.argv[2]), Number.parseInt(process.argv[3]))
-  );
+  // Parse command-line arguments to integers
+  const arg1 = Number.parseInt(process.argv[2]);
+  const arg2 = Number.parseInt(process.argv[3]);
+  
+  // Check if arguments are provided
+  if (process.argv.length < 4) {
+    console.log('Usage: ./script.js <integer1> <integer2>');
+  } else {
+    // Call 'add' function and print the result
+    console.log(add(arg1, arg2));
+  }
+  
