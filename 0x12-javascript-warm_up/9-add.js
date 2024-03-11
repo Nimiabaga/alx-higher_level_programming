05d8fb70a67c48be31078ebcd52e9262d435ec2e
nimiabaga <nimiabaga@yahoo.com>
2024-03-11 22:26:41 +0100

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-
-// Define func 'add' that takes two param. 'a' and 'b' and returns the sum
 function add (a, b) {
     return a + b;
   }
   
-  console.log(
-    add(Number.parseInt(process.argv[2]), Number.parseInt(process.argv[3]))
-  );
+  console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
   
