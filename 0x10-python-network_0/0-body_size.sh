bfa3b7fb4e67ea6275e756d77a09c2803d274f4d
nimiabaga <nimiabaga@yahoo.com>
2024-03-27 20:33:19 +0100

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script takes a URL as input, sends a request to that URLsize of the body of the response.
-
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+# Takes in a URL and sends a request,also displays the size of the body of the response
+curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}'
