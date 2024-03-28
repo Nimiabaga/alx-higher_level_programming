68b6a983c09fd6195d2799310b8eb950dde01b2f
nimiabaga <nimiabaga@yahoo.com>
2024-03-28 17:43:28 +0100

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,7 +2,7 @@
 """
 Python script that takes in a URL, sends
 a request to the URL and displays the body
-of the response. If the HTTP status 
+of the response. If the HTTP status
 code is greater than or equal to 400, it prints
 "Error code:" followed by the value of the HTTP status code.
 """
